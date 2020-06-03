f425ec0947f4b524568702bad52ccdfc92f1bcd1
a <26489386+fse1@users.noreply.github.com>
2020-06-03 11:34:26 -0400

    Task #32 - Added Shared Database Name to Database Connection Function

--- lib/database.php
+++ lib/database.php
@@ -2,22 +2,36 @@
 
 function connectToDatabase()
 {
-//login to sql
-//Change this to your connection info.
-$DATABASE_HOST = 'tethys.cse.buffalo.edu';
-$DATABASE_USER = 'FIXME';
-$DATABASE_PASS = 'FIXME';
-$DATABASE_NAME = 'FIXME';
-// Try and connect using the info above.
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-try {
-  $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-  if ( mysqli_connect_errno() ) {
-       // If there is an error with the connection, stop the script and display the error.
-       die ('Failed to connect to MySQL: ' . mysqli_connect_error());
+  
+  //login to sql
+  //Change this to your connection info.
+  $DATABASE_HOST = 'tethys.cse.buffalo.edu';
+  $DATABASE_USER = 'FIXME';
+  $DATABASE_PASS = 'FIXME';
+  $DATABASE_NAME = 'cse442_542_2020_summer_teamb_db';
+  
+  // Try and connect using the info above.
+  mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+  
+  try
+  {
+    
+    $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+    
+    if ( mysqli_connect_errno() )
+    {
+      
+     // If there is an error with the connection, stop the script and display the error.
+     die ('Failed to connect to MySQL: ' . mysqli_connect_error());
+         
+    }
+    
+    return $con;
+    
+  } 
+  catch (Exception $e)
+  {
+    die ('Failed to connect to MySQL: ' . $e->getMessage());
   }
-  return $con;
-} catch (Exception $e) {
- die ('Failed to connect to MySQL: ' . $e->getMessage());
-}
+
 }
