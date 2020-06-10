b20668a0f39ecba7cae2815e6b2662512bebe3a9
a <26489386+fse1@users.noreply.github.com>
2020-06-09 22:46:33 -0400

    added session validation function - #56

--- lib/infoClasses.php
+++ lib/infoClasses.php
@@ -90,8 +90,126 @@ class InstructorInfo
   // checks the value of the session cookie
   public function check_session($db_connection, $action)
   {
+    
+    // try to get the value of the session cookie
+    $session_cookie = NULL;
+    
+    if (isset($_COOKIE[SESSION_COOKIE_NAME]))
+    {
+      $session_cookie = $_COOKIE[SESSION_COOKIE_NAME];
+    }
+    else
+    {
+      $this->session_status = 2;
+      
+      if ($action == 0)
+      {
+        http_response_code(302);   
+        header("Location: instructorLogin.php");
+        exit();
+      }
+      else if ($action == 2)
+      {
+        http_response_code(403);   
+        echo "Forbidden";
+        exit();
+      }
+      
+      return;
+    }
+    
+    // hash the value in order to lookup the value stored in the database
+    $session_cookie = hex2bin($session_cookie);
+    
+    // check for errors
+    if (!$session_cookie)
+    {
+      $this->session_status = 2;
+      
+      if ($action == 0)
+      {
+        http_response_code(302);   
+        header("Location: instructorLogin.php");
+        exit();
+      }
+      else if ($action == 2)
+      {
+        http_response_code(403);   
+        echo "Forbidden";
+        exit();
+      }
+      
+      return;
+    }
+    
+    $session_cookie = hash_pbkdf2("sha256", $session_cookie, SESSIONS_SALT, PBKDF2_ITERS);
+    
+    // query the database for the associated instructor
+    $stmt = $db_connection->prepare('SELECT id, name, csrf_token, session_expiration FROM instructors WHERE session_token=?');
+    $stmt->bind_param('s', $session_cookie);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $data = $result->fetch_all(MYSQLI_ASSOC);
+    
+    // check for no match
+    if ($result->num_rows != 1)
+    {
+      $this->session_status = 2;
+      
+      if ($action == 0)
+      {
+        http_response_code(302);   
+        header("Location: instructorLogin.php");
+        exit();
+      }
+      else if ($action == 2)
+      {
+        http_response_code(403);   
+        echo "Forbidden";
+        exit();
+      }
+      
+      return;
+    }
+       
+    // populate the necessary data into the object
+    $this->id = $data[0]['id'];
+    $this->name = $data[0]['name'];
+    $this->csrf_token = $data[0]['csrf_token'];
+    $this->session_expiration = $data[0]['session_expiration'];
+    
+    // check the session expiration status
+    if (time() < $this->session_expiration)
+    {
+      $this->session_status = 0;
+    }
+    else
+    {
+      $this->session_status = 1;
+      
+      if ($action == 0)
+      {
+        http_response_code(302);   
+        header("Location: instructorLogin.php");
+        exit();
+      }
+      else if ($action == 2)
+      {
+        http_response_code(403);   
+        echo "Forbidden";
+        exit();
+      }
+    }
+    
+    // finally last redirect action
+    if ($action == 1)
+    {
+      http_response_code(302);   
+      header("Location: dashboard.php");
+      exit();
+    }
+      
   }
   
 }
-
 ?>
