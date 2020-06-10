3ae97f1a6d0c0051ef5b7fed8efcd366843b0162
a <26489386+fse1@users.noreply.github.com>
2020-06-10 11:15:37 -0400

    Added redirection to login page when trying to access a protected page with an invalid session cookie - #57

--- instructor/dashboard.php
+++ instructor/dashboard.php
@@ -10,24 +10,20 @@ ini_set("error_log", "~/php-error.log");
 session_start();
 
 // bring in required code
-require "../lib/database.php";
-require "../lib/constants.php";
+require_once "../lib/database.php";
+require_once "../lib/constants.php";
+require_once "../lib/infoClasses.php";
 
-// handle access code submissions
-if ($_SERVER['REQUEST_METHOD'] == 'POST')
-{
-  
-  // connect to the database
-  $con = connectToDatabase();
-  
-}
-//Check the presence of the init auth cookie. 
-//If cookie is not found, redirects user to instructorLogin page. 
-if(!isset($_COOKIE['init-auth'])){
-	header("Location: https://www-student.cse.buffalo.edu/CSE442-542/2020-Summer/cse-442b/production/instructor/instructorLogin.php");
-}
+
+// query information about the requester
+$con = connectToDatabase();
+
+// try to get information about the instructor who made this request by checking the session token and redirecting if invalid
+$instructor = new InstructorInfo();
+$instructor->check_session($con, 0);
+
+
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -37,7 +33,7 @@ if(!isset($_COOKIE['init-auth'])){
     <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-blue.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="../styles/nav-header.css">
-    <title>Instructor Webpage</title>
+    <title>UB CSE Peer Evaluation :: Dashboard</title>
 <ul> 
   <li><a href="dashboard.php">Home</a></li>
   <li><a href="surveys.php">Surveys</a></li>
