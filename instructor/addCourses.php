a9a0f31def0971590d4d8389dd287c1b33bb3623
a <26489386+fse1@users.noreply.github.com>
2020-06-11 08:50:12 -0400

    updated course addition form validation - #47

--- instructor/addCourses.php
+++ instructor/addCourses.php
@@ -22,81 +22,80 @@ $con = connectToDatabase();
 $instructor = new InstructorInfo();
 $instructor->check_session($con, 0);
 
-//allocates fields from this 'addCourse' form into an array
-$courseInfo = array('course-code','course-name','semester','course-year');
 
 //stores error messages corresponding to form fields
 $errorMsg = array();
 
-//flag for error messages
-$error = false;
-if(isset($_POST['add'])) {
+if($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+  
+  // make sure values exist
+  if (!isset($_POST['course-code']) or !isset($_POST['course-name']) or !isset($_POST['course-year']))
+  {
+    http_response_code(400);
+    echo "Bad Request: Missing parmeters.";
+    exit();
+  }
+  
+  //check valid formatting
+  $course_code = trim($_POST['course-code']);
+  if (empty($course_code))
+  {
+    $errorMsg['course-code'] = 'Course code cannot be blank.';
+  }
+  else if (!preg_match("/^[a-zA-Z0-9]*$/", $course_code))
+  {
+    $errorMsg["course-code"] = "Please enter a valid course code.";
+  }
+  
+  $course_name = trim($_POST['course-name']);
+  if (empty($course_name))
+  {
+    $errorMsg['course-name'] = 'Course name cannot be blank.';
+  }
+  else if (!preg_match("/^[a-zA-Z0-9\/&\-',\\s]*$/", $course_name))
+  {    
+    $errorMsg["course-name"] = "Please enter a valid course name.";
+  } 
 
-    foreach($courseInfo AS $info) {
-    
-        //check valid formatting
-        if ($info == 'course-code') {
-            
-            if (!preg_match("/^[a-zA-Z0-9]*$/",$_POST[$info])) {
-                //echo "<br>";
-                $errorMsg["course-code"] = "<span class=\"error\">Please enter a valid course code.</span>";
-                $error = true;
-            }
-
-
-        } elseif($info == 'course-name') {
-
-            if (preg_match("/^[a-zA-Z0-9\/&\-',\\s]*$/", $_POST[$info])) {
-                
-            } else {
-                //echo "<br>";
-                $errorMsg["course-name"] = "<span class=\"error\">Please enter a valid course name.</span>";
-                $error = true;
-
-            }
-        } elseif($info == 'semester') {
-            
-
-          //Prevent injections into 'semester' field
-          if ($_POST[$info] == "fall" || $_POST[$info] == "winter" || $_POST[$info] == "spring" || $_POST[$info] == "summer") {
-               
-          } else {
-             //echo "<br>";
-             $errorMsg["semester"] = "<span class=\"error\">Please enter a valid semester.</span>"; 
-             $error = true; 
-          }
-        
-        }
-
-        else {
-            
-            if ($info == 'course-year') {
-            
-                if(!preg_match("/^[0-9]*$/",$_POST[$info]) || strlen($_POST[$info]) != 4) {
-                    //echo "<br>";
-                    $errorMsg["course-year"] = "<span class=\"error\">Please enter a valid year.</span>";
-                    $error = true;
-                }
-
-            }   
-        }
-    
+  $semester = NULL;
+  if (!isset($_POST['semester']))
+  {
+    $errorMsg['semester'] = 'Please choose a semester.';
+  }
+  else
+  {
+    $semester = trim($_POST['semester']);         
+    if (empty($semester))
+    {
+      $errorMsg['semester'] = 'Please choose a semester.';
     }
-
-    if (!$error) {
-
-        if (isset($_POST[$course_code]) && isset($_POST[$course_name]) && isset($_POST[$semester]) && isset($_POST[$course_year]))  {
-        $stmt = $con -> prepare("INSERT INTO TestCourses (course_code,course_name,course_semester,course_year) VALUES ($_POST[$course_code], $_POST[$course_name], $_POST[$semester], $_POST[$course_year])");
-        $stmt -> execute();
-        echo "<script>alert('Your course was added sucessfully!');</script>";
-        exit();
-        }
-
-        
+    //Prevent injections into 'semester' field
+    else if ($semester != "fall" and $semester != "winter" and $semester != "spring" and $semester != "summer")
+    {
+       $errorMsg["semester"] = "Please select a valid semester."; 
     }
+  }
+  
+  $course_year = trim($_POST['course-year']);
+  if (empty($course_year))
+  {
+    $errorMsg['course-year'] = 'Course year cannot be blank.';
+  }
+  else if(!preg_match("/^[0-9]*$/",$course_year) || strlen($course_year) != 4)
+  {
+    $errorMsg["course-year"] = "Please enter a valid year.";
+  }
+
+  // check if fields are all valid
+  if (empty($errorMsg))
+  {
+    $stmt = $con->prepare("INSERT INTO TestCourses (course_code,course_name,course_semester,course_year) VALUES ($_POST[$course_code], $_POST[$course_name], $_POST[$semester], $_POST[$course_year])");
+    $stmt -> execute();
+    echo "<script>alert('Your course was added sucessfully!');</script>";
+  }
+
 }
-
-
 ?>
 
 <!DOCTYPE html>
@@ -118,18 +117,18 @@ if(isset($_POST['add'])) {
 
 <!--------form action="addCourses.php" once linked-------------------->
 <form action="addCourses.php" method ="post" class="w3-container">
-    <p><?php if(isset($errorMsg["course-code"])) {echo $errorMsg["course-code"];} ?></p>
+    <span class="w3-card w3-red"><?php if(isset($errorMsg["course-code"])) {echo $errorMsg["course-code"];} ?></span><br />
     <label for="course-code">Course Code:</label><br>
-    <input type="text" id="course-code" minlength=5 maxlength=6 class="w3-input w3-border w3-animate-input" style="width:30%" name="course-code" placeholder="e.g, CSE442" required><br>
+    <input type="text" id="course-code" class="w3-input w3-border" style="width:30%" name="course-code" placeholder="e.g, CSE442"><br>
     
 
-    <p><?php if(isset($errorMsg["course-name"])) {echo $errorMsg["course-name"];}  ?></p>
+    <span class="w3-card w3-red"><?php if(isset($errorMsg["course-name"])) {echo $errorMsg["course-name"];} ?></span><br />
     <label for="course-name">Course Name:</label><br>
-    <input type="text" id="course-name" class="w3-input w3-border w3-animate-input" style="width:30%" name="course-name" placeholder="e.g, Software Engineering Concepts" required><br>
+    <input type="text" id="course-name" class="w3-input w3-border" style="width:30%" name="course-name" placeholder="e.g, Software Engineering Concepts"><br>
 
-    <p><?php if(isset($errorMsg["semester"])) {echo $errorMsg["semester"];} ?></p>
+    <span class="w3-card w3-red"><?php if(isset($errorMsg["semester"])) {echo $errorMsg["semester"];} ?></span><br />
     <label for="semester">Course Semester:</label><br>
-    <select class="w3-select w3-border" style="width:30%" name="semester" required>
+    <select class="w3-select w3-border" style="width:30%" name="semester">
         <option value="" disabled selected>Choose semester:</option>
         <option value="fall">Fall</option>
         <option value="winter">Winter</option>
@@ -137,10 +136,10 @@ if(isset($_POST['add'])) {
         <option value="summer">Summer</option>
     </select><br><br>
 
-    <p><?php if(isset($errorMsg["course-year"])) {echo $errorMsg["course-year"];} ?></p>
+    <span class="w3-card w3-red"><?php if(isset($errorMsg["course-year"])) {echo $errorMsg["course-year"];} ?></span><br />
     <label for="year">Course Year:</label><br>
-    <input type="number" min="2020" id="year" class="w3-input w3-border w3-animate-input" style="width:30%" name="course-year" placeholder="e.g, 2020" required><br>
-    <input type="submit" name="add" value="Add">
+    <input type="number" id="year" class="w3-input w3-border" style="width:30%" name="course-year" placeholder="e.g, 2020"><br>
+    <input type="submit" value="Add Course">
 
 </form>
 </html>
