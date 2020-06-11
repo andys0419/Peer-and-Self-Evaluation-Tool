35058f146625663fb4cb5159ab7db08b30589148
a <26489386+fse1@users.noreply.github.com>
2020-06-10 20:46:50 -0400

    Added redirection to login page when trying to access a protected page with an invalid session cookie - #57

---
+++ instructor/courses.php
@@ -0,0 +1,26 @@
+<?php
+
+//error logging
+error_reporting(-1); // reports all errors
+ini_set("display_errors", "1"); // shows all errors
+ini_set("log_errors", 1);
+ini_set("error_log", "~/php-error.log");
+
+// start the session variable
+session_start();
+
+// bring in required code
+require_once "../lib/database.php";
+require_once "../lib/constants.php";
+require_once "../lib/infoClasses.php";
+
+
+// query information about the requester
+$con = connectToDatabase();
+
+// try to get information about the instructor who made this request by checking the session token and redirecting if invalid
+$instructor = new InstructorInfo();
+$instructor->check_session($con, 0);
+
+
+?>
