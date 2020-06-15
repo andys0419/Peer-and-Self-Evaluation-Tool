05a953c77ed6779c66ec351e79e59d4985eeebb1
uladzimi <uladzimi@buffalo.edu>
2020-06-15 15:48:48 -0400

    #18 added initial variables

--- instructor/addSurveys.php
+++ instructor/addSurveys.php
@@ -1,3 +1,43 @@
+<?php
+
+//error logging
+error_reporting(-1); // reports all errors
+ini_set("display_errors", "1"); // shows all errors
+ini_set("log_errors", 1);
+ini_set("error_log", "~/php-error.log");
+
+//start the session variable
+session_start();
+
+//bring in required code
+require_once "../lib/database.php";
+require_once "../lib/constants.php";
+require_once "../lib/infoClasses.php";
+
+
+//query information about the requester
+$con = connectToDatabase();
+
+//try to get information about the instructor who made this request by checking the session token and redirecting if invalid
+$instructor = new InstructorInfo();
+$instructor->check_session($con, 0);
+
+
+//stores error messages corresponding to form fields
+$errorMsg = array();
+
+// set flags
+$survey_type = NULL;
+$course_id = NULL;
+$rubric_id = NULL;
+$pairing_file = NULL;
+$start_date = NULL;
+$end_date = NULL;
+$start_time = NULL;
+$end_time = NULL;
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
