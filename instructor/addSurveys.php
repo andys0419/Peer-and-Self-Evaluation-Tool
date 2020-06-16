a7b4cd89b838b0bd31aac0ac9d46320aaf397598
a <26489386+fse1@users.noreply.github.com>
2020-06-15 21:40:11 -0400

    Added pairing file and pairing file mode to survey creation form - #80

--- instructor/addSurveys.php
+++ instructor/addSurveys.php
@@ -27,7 +27,7 @@ $instructor->check_session($con, 0);
 $errorMsg = array();
 
 // set flags
-$survey_type = NULL;
+$course = NULL;
 $course_id = NULL;
 $rubric_id = NULL;
 $pairing_file = NULL;
@@ -35,9 +35,9 @@ $start_date = NULL;
 $end_date = NULL;
 $start_time = NULL;
 $end_time = NULL;
+$pairing_mode = NULL;
 
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -51,20 +51,19 @@ $end_time = NULL;
 </head>
 <body>
 
-    <div class="w3-container w3-center">
-        <h2>Survey Information</h2>
-    </div>
+<div class="w3-container w3-center">
+    <h2>Survey Information</h2>
+</div>
 
-<div id="columns">
-<div id="left">
+
 <span class="w3-card w3-red"><?php if(isset($errorMsg["duplicate"])) {echo $errorMsg["duplicate"];} ?></span>
-<form action="addCourses.php" method ="post" class="w3-container">
-    <span class="w3-card w3-red"><?php if(isset($errorMsg["survey-type"])) {echo $errorMsg["survey-type"];} ?></span><br />
-    <label for="survey-type">Survey Type:</label><br>
-    <select class="w3-select w3-border" style="width:61%" name="survey-type">
-        <option value="" disabled <?php if (!$survey_type) {echo 'selected';} ?>>Choose Survey:</option>
-        <option value="FIXME" <?php if ($survey_type == 1) {echo 'selected';} ?>>placeholder</option>
-        <option value="FIXME" <?php if ($survey_type == 2) {echo 'selected';} ?>>placeholder</option>
+<form action="addSurveys.php" method ="post" enctype="multipart/form-data" class="w3-container">
+    <span class="w3-card w3-red"><?php if(isset($errorMsg["course"])) {echo $errorMsg["course"];} ?></span><br />
+    <label for="course">Course:</label><br>
+    <select class="w3-select w3-border" style="width:61%" name="course">
+        <option value="0" disabled <?php if (!$course_id) {echo 'selected';} ?>>Select Course</option>
+        <option value="FIXME" <?php if ($course_id == 1) {echo 'selected';} ?>>placeholder</option>
+        <option value="FIXME" <?php if ($course_id == 2) {echo 'selected';} ?>>placeholder</option>
     </select><br><br>
 
     <span class="w3-card w3-red"><?php if(isset($errorMsg["rubric-id"])) {echo $errorMsg["rubric-id"];} ?></span><br />
@@ -82,20 +81,24 @@ $end_time = NULL;
     <span class="w3-card w3-red"><?php if(isset($errorMsg["start-time"])) {echo $errorMsg["start-time"];} ?></span><br />
     <label for="start-time">Start time:</label><br>
     <input type="time" id="start-time" class="w3-input w3-border" style="width:61%" name="start-time" placeholder="e.g., 09:05:PM" <?php if ($start_time) {echo 'value="' . htmlspecialchars($start_time) . '"';} ?>><br>
-    </div>
 
-<div id="right">
+
     <span class="w3-card w3-red"><?php if(isset($errorMsg["course-id"])) {echo $errorMsg["course-id"];} ?></span><br />
-    <label for="course-id">Question Bank:</label><br>
-    <select class="w3-select w3-border" style="width:61%" name="course-id">
-        <option value="" disabled <?php if (!$course_id) {echo 'selected';} ?>>Whateveritdoes:</option>
-        <option value="FIXME" <?php if ($course_id == 1) {echo 'selected';} ?>>placeholder</option>
-        <option value="FIXME" <?php if ($course_id == 2) {echo 'selected';} ?>>placeholder</option>
+    <label for="rubric-id">Question Bank:</label><br>
+    <select class="w3-select w3-border" style="width:61%" name="rubric-id" id="rubric-id" disabled>
+        <option value="0" selected>Default</option>
     </select><br><br>
 
+    <span class="w3-card w3-red"><?php if(isset($errorMsg["pairing-mode"])) {echo $errorMsg["pairing-mode"];} ?></span><br />
+    <label for="pairing-mode">Pairing File Mode:</label><br>
+    <select id="pairing-mode" class="w3-select w3-border" style="width:61%" name="pairing-mode">
+        <option value="1" selected>Raw</option>
+        <option value="2">Team</option>
+    </select><br><br>
+    
     <span class="w3-card w3-red"><?php if(isset($errorMsg["pairing-file"])) {echo $errorMsg["pairing-file"];} ?></span><br />
-    <label for="pairing-file">Pairings:</label><br>
-    <input type="text" id="pairing-file" class="w3-input w3-border" style="width:61%" name="pairing-file" placeholder="e.g, data.csv" <?php if ($pairing_file) {echo 'value="' . htmlspecialchars($pairing_file) . '"';} ?>><br>
+    <label for="pairing-file">Pairings (CSV File):</label><br>
+    <input type="file" id="pairing-file" class="w3-input w3-border" style="width:61%" name="pairing-file" placeholder="e.g, data.csv" <?php if ($pairing_file) {echo 'value="' . htmlspecialchars($pairing_file) . '"';} ?>><br>
     
 
     <span class="w3-card w3-red"><?php if(isset($errorMsg["end-date"])) {echo $errorMsg["end-date"];} ?></span><br />
@@ -105,8 +108,8 @@ $end_time = NULL;
     <span class="w3-card w3-red"><?php if(isset($errorMsg["end-time"])) {echo $errorMsg["end-time"];} ?></span><br />
     <label for="end-time">End time:</label><br>
     <input type="time" id="end-time" class="w3-input w3-border" style="width:61%" name="end-time" placeholder="e.g., 11:59:PM" <?php if ($end_time) {echo 'value="' . htmlspecialchars($end_time) . '"';} ?>><br>
-    </div>
-</div>
-    <p id="submission"><input type="submit" value="Add Survey"></p>
+
+    <input type="submit" class="w3-button w3-blue" value="Create Survey">
 </form>
+</body>
 </html>
