c22b8ba225bd86020cc34b864b72fb2b2ec87dc8
a <26489386+fse1@users.noreply.github.com>
2020-06-16 13:52:42 -0400

    Added basic team parsing to the parser function - #81

--- lib/fileParse.php
+++ lib/fileParse.php
@@ -5,11 +5,18 @@ function parse_pairings($mode, $data_fields)
 {
   
   // return array
-  $split = NULL;
+  $split = array();
   
   // handle team mode or roster mode
   if ($mode == "1")
   {
+    
+    // check for blank file
+    if (empty(trim($data_fields)))
+    {
+      $split['error'] = 'Input CSV file cannot be blank.';
+      return $split;
+    }
     
     // split on the comma
     $split = explode(",", $data_fields);
@@ -39,6 +46,53 @@ function parse_pairings($mode, $data_fields)
   }
   else if ($mode == "2")
   {
+    
+    // check for blank file
+    if (empty(trim($data_fields)))
+    {
+      $split['error'] = 'Input CSV file cannot be blank.';
+      return $split;
+    }
+    
+    // split be newlines
+    $lines = explode("\n", $data_fields);
+    
+    // examine each line or team
+    $line_count = count($lines);
+    
+    for ($j = 0; $j < $line_count; $j++)
+    {
+      
+      // skip over blank lines
+      $lines[$j] = trim($lines[$j]);
+      if (empty($lines[$j]))
+      {
+        continue;
+      }
+      
+      // split on the comma
+      $temp_split = explode(",", $lines[$j]);
+      $size = count($temp_split);
+      
+      // remove whitespace from each entry and reject if any fields are blank
+      for ($i = 0; $i < $size; $i++)
+      {
+        $temp_split[$i] = trim($temp_split[$i]);
+        
+        if (empty($temp_split[$i]))
+        {
+          $split['error'] = 'Input CSV file cannot have any blank fields.';
+          return $split;
+        }
+      }
+      
+      // add the team to the array
+      array_push($split, $temp_split);
+    
+    }
+    
+    return $split;
+    
   }
   
 }
