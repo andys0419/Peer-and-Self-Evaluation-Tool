af61c660e80fa590da452425bdcb0eb9177a6ca1
uladzimi <uladzimi@buffalo.edu>
2020-06-17 19:30:21 -0400

    #87 fileParse supports roster mode

--- lib/fileParse.php
+++ lib/fileParse.php
@@ -39,6 +39,45 @@ function parse_pairings($mode, $data_fields)
   }
   else if ($mode == "2")
   {
+
+    // split on the comma
+    $split = explode(",", $data_fields);
+    
+    // reject if there are not an even number of fields
+    $size = count($split);
+    if ($size % 2 !== 0)
+    {
+      $split['error'] = 'Input CSV file must have an even number of fields for raw mode.';
+      return $split;
+    }
+    
+    // remove whitespace from each entry and reject if any fields are blank
+    for ($i = 0; $i < $size; $i++)
+    {
+      $split[$i] = trim($split[$i]);
+      
+      if (empty($split[$i]))
+      {
+        $split['error'] = 'Input CSV file cannot have any blank fields.';
+        return $split;
+      }
+
+      if ($i%2 == 0 && !ctype_alpha($split[$i]) )
+      {
+        $split['error'] = 'Names can only contain letters.';
+        return $split;
+      }
+
+      if ($i%2 !== 0 && !filter_var($split[$i], FILTER_VALIDATE_EMAIL) )
+      {
+        $split['error'] = 'Invalid email.';
+        return $split;
+      }
+
+    }
+    
+    return $split;
+
   }
   
 }
