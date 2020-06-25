81768338be6415b003fe0fc9b2b92f1e7dd96548
ashi0419 <andyshi@buffalo.edu>
2020-06-25 13:57:12 -0400

    Removed reference to dashboard.php #109

--- instructor/courses.php
+++ instructor/courses.php
@@ -60,8 +60,6 @@ while ($row = $result->fetch_assoc())
 <body>
 
  <div class="icon-bar">
-  <a href="dashboard.php"><i class="home"><img src="../icons/home.png" width="50" height="50" class="img-center">Home</i></a>
-
   <a href="surveys.php"><i class="survey"><img src ="../icons/survey.png" width="50" height="50" class="img-center"> Surveys</i></a>
   
   <a href="question-banks.php" class="disable"><i class="question"><img src="../icons/check.png" width="50" height="50"  class="img-center"> Question Banks </i></a>
