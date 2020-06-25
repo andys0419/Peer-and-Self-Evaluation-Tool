03281ce7c78c22311bd016de6eed2f134dbbf15e
ashi0419 <andyshi@buffalo.edu>
2020-06-25 16:19:52 -0400

    Enable button 'add survey' to be more mobile friendly #116

--- instructor/surveys.php
+++ instructor/surveys.php
@@ -152,10 +152,13 @@ foreach($courses as $course) {
           }
       ?>
     </table>
-<body>
 
 <div class = "w3-center w3-mobile">
     <a href="addSurveys.php"><button class="w3-button w3-blue">+ Add Survey</button></a>
 </div> 
+
+<body>
+
+
 
 </html> 
